162937298052b1a331b6eddc4e23da4dc49545e0
fukioston <2112966@mail.nankai.edu.cn>
2023-10-11 14:06:02 +0800

    修复了n个bug

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var connection=require('../db/sql.js')
+const db=require('../db/sql.js')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+router.get('/api/goods/search', function(req, res, next) {
+  db.query("select * from goods",function(error,results,fields){
+	  if(error) throw error;
+	  console.log(results[0]);
+  } )
 });
 
 module.exports = router;
