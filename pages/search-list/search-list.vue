bad2ce7e2db04721b221089b2eb16abe1a561f81
fukioston <2112966@mail.nankai.edu.cn>
2023-10-12 11:52:46 +0800

    提交一些乱七八糟的

---
+++ pages/search-list/search-list.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		搜索结果页面
+	</view>
+</template>
+
+<script>
+	// import ShopList from '@components'
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
