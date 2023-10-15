29b1229f43a43a6abea8023998fb4bfb455733d6
drphandom <2387378739@qq.com>
2023-10-16 00:09:59 +0800

    登录，个人中心初步界面

---
+++ pages/login/login.vue
@@ -0,0 +1,86 @@
+<template>
+  <view class="login-page">
+    <view class="login-form">
+      <h1>Login</h1>
+      <form @submit.prevent="login">
+        <view class="form-group">
+          <label for="username">Username:</label>
+          <input type="text" id="username" v-model="username" required>
+        </view>
+        <view class="form-group">
+          <label for="password">Password:</label>
+          <input type="password" id="password" v-model="password" required>
+        </view>
+        <button type="submit">Log In</button>
+      </form>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    login() {
+      // 在此处添加处理登录逻辑的代码
+      // 通常，您将发送登录请求到后端并验证用户凭据
+      // 如果登录成功，可以导航到其他页面
+    }
+  }
+};
+</script>
+
+<style scoped>
+.login-page {
+  background-image: url("/static/img/swiper.jpg");
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width:100%;
+}
+
+.login-form {
+  text-align: center;
+  background: #f3f3f3;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  height: 100%;
+  width:100%;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.form-group {
+  margin: 10px 0;
+}
+
+label {
+  font-weight: bold;
+}
+
+input {
+  padding: 5px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+button {
+  background: #007bff;
+  color: white;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,8 +1,155 @@
 <template>
+	<view class="user-center">
+		<view class="setting-corns">这里是一些设置</view>
+		<view class="user-info">
+			<img class="avatar" :src="user.avatar" alt="User Avatar">
+			<view class="user-details">
+				<h2>{{ user.name }}</h2>
+				<p>{{ user.bio }}</p>
+			</view>
+		</view>
+		<view class="relations">
+			<view class="concern">关注</view>
+			<view class="mysale">我的商品</view>
+			<view class="fans">粉丝</view>
+		</view>
+		<view class="myorder">我的订单</view>
+		<view class="myfrint">浏览记录</view>
+		<view class="kefu">联系客服</view>
+		<view class="setting">设置</view>
+	</view>
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				user: {
+					name: "用户名", // 用户名
+					avatar: "ava.jpg", // 头像图片路径
+					bio: "个人简介" // 个人简介
+				}
+			};
+		},
+		methods: {
+			logout() {
+				// 在这里执行退出登录的逻辑，例如清除用户会话信息等
+				// 跳转到登录页或其他适当的操作
+			}
+		}
+	};
 </script>
 
-<style>
+<style scoped>
+	.setting-corns {
+		
+	}
+
+	.user-center {
+		background-color: azure;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding: 20px;
+	}
+
+	.user-info {
+		display: flex;
+		align-items: center;
+	}
+
+	.avatar {
+		width: 80px;
+		height: 80px;
+		border-radius: 50%;
+		margin-right: 20px;
+	}
+
+	.user-details {
+		text-align: left;
+	}
+
+	.relations {
+		width: 100%;
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.concern {
+		padding-top: 4%;
+		padding-bottom: 4%;
+		width: 33%;
+		border: 1px solid #ccc;
+		flex-direction: row;
+		align-items: center;
+		flex: 10;
+		display: flex;
+		justify-content: center;
+	}
+
+	.mysale {
+		padding-top: 4%;
+		padding-bottom: 4%;
+		border: 1px solid #ccc;
+		width: 33%;
+		flex: 10;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.fans {
+		padding-top: 4%;
+		padding-bottom: 4%;
+		border: 1px solid #ccc;
+		width: 33%;
+		flex: 10;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+	}
+	.myorder{
+		
+		margin:5%;
+		margin-top:10%;
+		width: 100%;
+		align-items: center;
+		justify-content: center;
+		font-size:20px;
+		display: flex;
+		justify-content: space-between;
+		padding-left:10%;
+	}
+	.myfrint{
+		margin:5%;
+		width: 100%;
+		align-items: center;
+		justify-content: center;
+		font-size:20px;
+		display: flex;
+		justify-content: space-between;
+		padding-left:10%;
+	}
+	.kefu{
+		margin:5%;
+		width: 100%;
+		align-items: center;
+		justify-content: center;
+		font-size:20px;
+		display: flex;
+		justify-content: space-between;
+		padding-left:10%;
+	}
+	.setting{
+		margin:5%;
+		width: 100%;
+		align-items: center;
+		justify-content: center;
+		font-size:20px;
+		display: flex;
+		justify-content: space-between;
+		padding-left:10%;
+	}
 </style>
